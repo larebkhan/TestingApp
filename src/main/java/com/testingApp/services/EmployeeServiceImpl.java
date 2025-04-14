f9cd992d44c6146925b124c2e727bf36fd52be4e
Lareb <lareb131213@gmail.com>
2025-04-14 18:56:58 +0530

    added some tests for employeeRespository and service layer

---
+++ src/main/java/com/testingApp/services/EmployeeServiceImpl.java
@@ -0,0 +1,105 @@
+package com.testingApp.services;
+
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.util.ReflectionUtils;
+import org.springframework.stereotype.Service;
+
+import com.testingApp.dto.EmployeeDTO;
+import com.testingApp.entities.EmployeeEntity;
+import com.testingApp.exceptions.ResourceNotFoundException;
+import com.testingApp.repositories.EmployeeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+    private final EmployeeRepository employeeRepository;
+    private final ModelMapper modelMapper;
+
+    
+
+
+    public EmployeeDTO getEmployeeById(Long id) {
+        log.info("Fetching employee with id: {}", id);
+//        Optional<EmployeeEntity> employeeEntity = employeeRepository.findById(id);
+//        return employeeEntity.map(employeeEntity1 -> modelMapper.map(employeeEntity1, EmployeeDTO.class));
+        EmployeeEntity employee = employeeRepository.findById(id).orElseThrow(() -> {
+            log.error("Employee not found with id : {}", id);
+            return new ResourceNotFoundException("Employee not found with id : " + id);
+        });
+        log.info("Employee found with id: {}", id);
+        return modelMapper.map(employee, EmployeeDTO.class);
+    }
+
+    public List<EmployeeDTO> getAllEmployees() {
+        List<EmployeeEntity> employeeEntities = employeeRepository.findAll();
+        return employeeEntities
+                .stream()
+                .map(employeeEntity -> modelMapper.map(employeeEntity, EmployeeDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public EmployeeDTO createNewEmployee(EmployeeDTO inputEmployee) {
+//        to check if user is admin
+//        log something
+        log.info("Creating a new employee");
+        List<EmployeeEntity> employeeEntity = employeeRepository.findByEmail(inputEmployee.getEmail());
+        if(!employeeEntity.isEmpty()){
+            log.error("Employee already present with that email");
+            throw new RuntimeException("Employee already present with that email");
+        }
+        EmployeeEntity toSaveEntity = modelMapper.map(inputEmployee, EmployeeEntity.class);
+        EmployeeEntity savedEmployeeEntity = employeeRepository.save(toSaveEntity);
+        return modelMapper.map(savedEmployeeEntity, EmployeeDTO.class);
+    }
+
+    public EmployeeDTO updateEmployeeById(Long employeeId, EmployeeDTO employeeDTO) {
+        isExistsByEmployeeId(employeeId);
+        EmployeeEntity employeeEntity = modelMapper.map(employeeDTO, EmployeeEntity.class);
+        employeeEntity.setId(employeeId);
+        EmployeeEntity savedEmployeeEntity = employeeRepository.save(employeeEntity);
+        return modelMapper.map(savedEmployeeEntity, EmployeeDTO.class);
+    }
+
+    public void isExistsByEmployeeId(Long employeeId) {
+        boolean exists = employeeRepository.existsById(employeeId);
+        if(!exists) throw new ResourceNotFoundException("Employee not found with id: "+employeeId);
+    }
+
+    public boolean deleteEmployeeById(Long employeeId) {
+        isExistsByEmployeeId(employeeId);
+        employeeRepository.deleteById(employeeId);
+        return true;
+    }
+
+    public EmployeeDTO updatePartialEmployeeById(Long employeeId, Map<String, Object> updates) {
+        isExistsByEmployeeId(employeeId);
+        EmployeeEntity employeeEntity = employeeRepository.findById(employeeId).get();
+        updates.forEach((field, value) -> {
+            Field fieldToBeUpdated = ReflectionUtils.findRequiredField(EmployeeEntity.class, field);
+            fieldToBeUpdated.setAccessible(true);
+            ReflectionUtils.setField(fieldToBeUpdated, employeeEntity, value);
+        });
+        return modelMapper.map(employeeRepository.save(employeeEntity), EmployeeDTO.class);
+    }
+}
+
+
+
+
+
+
+
+
+
